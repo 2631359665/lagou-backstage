bfbde4b7f92be4f3108455ac63255b5ed0adbf61
2631359665 <2631359665@qq.com>
2020-08-10 12:08:00 +0800

    呵呵

--- lagou-admin-fe/src/router/active.js
+++
@@ -1,8 +0,0 @@
-export default function Active(req) {
-    let url = req.url;
-    // 当前需要选中的
-    // console.log(req.url);
-    let li = $("#menu-container li").find(`a[href='#${url}']`).parent();
-    li.addClass("active").siblings().removeClass("active");
-
-}
